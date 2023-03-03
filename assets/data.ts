c62e8d2ac7473449ba9ebea447da2896f3e4f947
frentzen <frentzenbenny@gmil.com>
2023-03-03 10:32:33 +0530

    Auto-generated commit [ci skip]

---
+++ assets/data.ts
@@ -0,0 +1,34 @@
+export const data = [
+    {title: "Trivandrum",
+image: "d3.jpg"},
+    {title: "Kollam",
+    image: "d1.jpg"},
+    {title: "Pathanamthitta",
+    image: "d4.jpg"},
+    {title: "Alappuzha",
+    image: "d5.jpg"},
+    {title: "Kottayam",
+    image: "d6.jpg"},
+    {title: "Idukki",
+    image: "d7.jpg"},
+    {title: "Ernakulam",
+    image: "d8.jpg"},
+    {title: "Thrissur",
+    image: "d9.jpg"},
+    {title: "Palakkad",
+    image: "d10.jpg"},
+    {title: "Malappuram",
+    image: "d11.jpg"},
+    {title: "Kozhikode",
+    image: "d12.jpg"},
+    {title: "Wayanad",
+image: "d1.jpg"},
+    {title: "Kannur",
+    image: "d13.jpg"},
+    {title: "Kasaragod",
+    image: "d11.jpg"}
+
+
+
+
+]
